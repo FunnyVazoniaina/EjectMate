ab9804734a6a1510c578b00499a258c8b4978557
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-07-22 10:53:46 +0200

    Feat: add Burntoast module notification

--- autoEjectUsb.bat
+++ autoEjectUsb.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 
+REM Parcourir tous les lecteurs amovibles
 for /f "tokens=1 delims==" %%D in ('wmic logicaldisk where "DriveType=2" get DeviceID /value ^| find "="') do (
     set "drive=%%D"
     call :ejectDrive %%D
@@ -8,7 +9,16 @@ for /f "tokens=1 delims==" %%D in ('wmic logicaldisk where "DriveType=2" get Dev
 goto :eof
 
 :ejectDrive
-powershell -Command "Get-WmiObject -Class Win32_Volume -Filter \"DriveLetter='%1'\" | ForEach-Object { $_.Dismount($false, $false); $_.Remove($false) }"
-echo %1 ejected.
-REM Optionally notify here with BurntToast or a MsgBox
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+    "$drive = '%1';" ^
+    "$vol = Get-WmiObject -Class Win32_Volume -Filter \"DriveLetter='$drive'\";" ^
+    "if ($vol) {" ^
+    "    $vol.Dismount($false, $false) | Out-Null;" ^
+    "    $vol.Remove($false) | Out-Null;" ^
+    "    Import-Module BurntToast;" ^
+    "    New-BurntToastNotification -Text 'USB Ejected', \"Drive $drive has been safely ejected.\";" ^
+    "} else {" ^
+    "    Import-Module BurntToast;" ^
+    "    New-BurntToastNotification -Text 'Ejection Failed', \"Drive $drive could not be found.\";" ^
+    "}"
 goto :eof
